c4136db870b4f4e2c63e6e80423786edd9645927
ilogic-8-a <urypin2010@gmail.com>
2024-12-12 17:57:30 +0300

    пцрцоуо

--- index3/position/задания/test/proekt.js
+++ index3/position/задания/test/proekt.js
@@ -3,6 +3,40 @@ function appendValue(value) {
     input.value = input.value + value;
 }
 
+function append(value) {
+    input.value = input.value - value;
+}
+
+function app(value) {
+    input.value = input.value / value;
+}
+
+function ap(value) {
+    input.value = input.value * value;
+}
+
+function clearInput() {
+    input.value = '';
+}
+
 function compute() {
-input.value = eval(input.value); }
+    let newValue = input.value.replace('/', '/');
+    newValue = input.value.replace('*', '*');
+    try {
+        input.value = eval(newValue);
+    }
+    catch(e) {
+        input.value = 'Ошибка!'
+    }
+}
+
+function deleteKey(event) {
+    console.log(event)
+    let keys = [ '1', '2', '3', '4', '5' ]
+    if (keys.includes(event.key) == false) {
+        event.preventDefault();
+    }
+}
+
+input.addEventListener('keypress', deleteKey);
 
