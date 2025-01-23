dccef96f37dfae114c9a895cd05dc7c18e204050
ilogic-8-a <urypin2010@gmail.com>
2025-01-23 18:04:52 +0300

    решали контрольную

--- javascript/array/index.js
+++ javascript/array/index.js
@@ -1,5 +1,5 @@
-// Массивы в JavaScript
-
+// // Массивы в JavaScript
+//
 let users = ['Иван', 'Павел', 'Анна' ]
 
 // Получение элемента из массива
@@ -23,14 +23,14 @@ users.unshift('Босс');
 // Удаление элемента из начала массива
 users.shift();
 
-// Удаление элемента по индексу
-// splice(индекс для удаления, кол-во элементов)
-// users.splice(1, 2)
-
+// // Удаление элемента по индексу
+// // splice(индекс для удаления, кол-во элементов)
+// // users.splice(1, 2)
+//
 console.log(users, users.length);
-
-// Объекты в JavaScript
-
+//
+// // Объекты в JavaScript
+//
 users = [
     {
         avatar: 'https://cdn1.flamp.ru/baa6d870a72fea7fd70b4b866cd456fe.png',
@@ -54,8 +54,8 @@ users = [
         birthday: '08.06.1996',
     },
 ]
-
-// Получение значения объекта по его ключу
+//
+// // Получение значения объекта по его ключу
 let firstUser = users[0];
 
 console.log('Первый пользователь:', firstUser);
@@ -71,47 +71,121 @@ delete firstUser['likes birds'];
 console.log(users)
 
 let usersList = document.getElementById("users-list");
-
-// Функция для вывода пользователей на экран
+//
+// // Функция для вывода пользователей на экран
 function showUsers() {
-    // Создаем карточка пользователя
-    let userCard = document.createElement('div');
-    // Создаем аватар пользователя
-    let userAvatar = document.createElement('div');
-    // Создаем имя пользователя
-    let userName = document.createElement('h2');
-    // Создаем номер телефона пользователя
-    let userPhone = document.createElement('p');
-    // Создаем почту пользователя
-    let userEmail = document.createElement('p');
-    // Создаем дату рождения пользователя
-    let userBirthday = document.createElement('p');
+    // Циклы в JavaScript
+    // for (let элемент_массива of массив) {
+    //  действия для выполнения над элементом
+    // }
+    usersList.innerHTML = ''
+    for (let user of users) {
+        // Создаем карточка пользователя
+        let userCard = document.createElement('div');
+        // Создаем аватар пользователя
+        let userAvatar = document.createElement('div');
+        // Создаем имя пользователя
+        let userName = document.createElement('h2');
+        // Создаем номер телефона пользователя
+        let userPhone = document.createElement('p');
+        // Создаем почту пользователя
+        let userEmail = document.createElement('p');
+        // Создаем дату рождения пользователя
+        let userBirthday = document.createElement('p');
+        // Добавляем к карточке пользователя класс user-card
+        userCard.classList.add('user-card');
+        // Добавляем к карточке пользователя класс user-avatar
+        userAvatar.classList.add('user-avatar')
+        // Добавляем к карточке пользователя класс user-name
+        userName.classList.add('user-name');
 
 
-    // Добавляем к карточке пользователя класс user-card
-    userCard.classList.add('user-card');
-    // Добавляем к карточке пользователя класс user-avatar
-    userAvatar.classList.add('user-avatar')
-    // Добавляем к карточке пользователя класс user-name
-    userName.classList.add('user-name');
+        // Поместим картинку внутрь div-'ktvtynf
+        userAvatar.innerHTML = `<img src="${user.avatar}"  alt="">`
+        // Поместим имя внутрь userName
+        userName.innerHTML = user.name;
+        // Поместим телефон внутрь userPhone
+        userPhone.innerHTML = `<b>Телефон:</b> ${user.phone}`;
+        // Поместим почту пользователя внутрь userEmail
+        userEmail.innerHTML = `<b>Email:</b> ${user.email}`;
+        // Поместим др внутрь userBirthday
+        userBirthday.innerHTML = `<b>День рождения</b> ${user.birthday}`;
 
+        // Поместим аватар пользователя внутрь созданной карточки
+        userCard.append(userAvatar, userName, userPhone, userEmail, userBirthday);
 
-    // Поместим картинку внутрь div-'ktvtynf
-    userAvatar.innerHTML = `<img src="${ firstUser.avatar}"  alt="">`
-    // Поместим имя внутрь userName
-    userName.innerHTML = firstUser.name;
-    // Поместим телефон внутрь userPhone
-    userPhone.innerHTML = `<b>Телефон:</b> ${ firstUser.phone}`;
-    // Поместим почту пользователя внутрь userEmail
-    userEmail.innerHTML = `<b>Email:</b> ${ firstUser.email}`;
-    // Поместим др внутрь userBirthday
-    userBirthday.innerHTML = `<b>День рождения</b> ${ firstUser.birthday}`;
-
-    // Поместим аватар пользователя внутрь созданной карточки
-    userCard.append(userAvatar, userName, userPhone, userEmail, userBirthday);
-
-    // Поместить карточку пользователя в конец списка
-    usersList.append(userCard);
+        // Поместить карточку пользователя в конец списка
+        usersList.append(userCard);
+    }
 }
 
 showUsers();
+
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10]
+//
+// for (let num of numbers) {
+//     console.log('Элемент массива', num)
+//     num = num + 1
+// }
+
+users = [
+    {
+        avatar: 'https://cdn1.flamp.ru/baa6d870a72fea7fd70b4b866cd456fe.png',
+        name: 'Иван',
+        email: 'ivan@mail.ru',
+        phone: '+7 (999) 143-143-14',
+        birthday: '07.02.1998',
+    },
+]
+
+function addInfo() {
+    // let info = document.createElement('div')
+    // let name = document.createElement('div');
+    // let phone = document.createElement('div');
+    // let email = document.createElement('div');
+    // let birthday = document.createElement('div');
+    // let addButton = document.createElement('button');
+    // button.addEventListener('click', addInfo);
+    let user = {
+        avatar: 'https://cdn1.flamp.ru/baa6d870a72fea7fd70b4b866cd456fe.png',
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        birthday: birthday.value,
+    }
+    users.push(user)
+    showUsers()
+}
+
+let info = document.getElementById("info")
+let name = document.getElementById("name");
+let phone = document.getElementById("phone");
+let button = document.getElementById("button");
+let email = document.getElementById("email");
+let birthday = document.getElementById("birthday");
+let list = document.getElementById("list");
+
+button.addEventListener("click", addInfo);
+
+
+    // Добавляем css классы на элементы
+    name.classList.add("name");
+    phone.classList.add("phone");
+    email.classList.add("pochta");
+    birthday.classList.add("birthday");
+
+    name.innerHTML = ` ${ name.value }`
+    phone.innerHTML = `Телефон: ${ phone.value }`
+    email.innerHTML = `Email: ${ email.value }`
+    birthday.innerHTML = `День рождения: ${ birthday.value}`
+    list.append(name, phone, email, birthday, );
+    // Добавление нового элемента в конец элемента
+    list.append(info);
+
+//     title.value = '';
+//     description.value = '';
+// }
+// button.addEventListener("click", addTask);
+
+
