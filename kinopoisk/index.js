a0608c7c9762b29612808aee1c45f8692110907e
ilogic-8-a <urypin2010@gmail.com>
2025-02-06 17:59:18 +0300

    решали контрольную

--- kinopoisk/index.js
+++ kinopoisk/index.js
@@ -1,9 +1,13 @@
 let filmsList = document.getElementById("movies");
+let count = 1;
 
 let films = [];
 
 async function getFilms() {
     let response = await axios.get('https://kinopoiskapiunofficial.tech/api/v2.2/films?type=TV_SERIES', {
+        params: {
+            page: count,
+        },
         headers: {
             'X-API-KEY': 'c5d1b7b1-4b6f-41e9-855a-292c1e102820',
             'Content-Type': 'application/json',
@@ -20,22 +24,45 @@ function showFilms() {
         let filmRating = document.createElement('div');
         let filmPoster = document.createElement('div');
         let filmName = document.createElement('div');
-        let filmYear = document.createElement('div');
 
-        filmRating.innerHTML = `${film.ratingKinopoisk}`;
-        filmPoster.innerHTML = `<img src = "${film.posterUrl}"  alt = ''>`
-        filmName.innerHTML = `${film.nameRu}`;
-        filmYear.innerHTML = `${film.year}`;
+        filmRating.innerHTML = `${ film.ratingKinopoisk }`;
+        filmPoster.innerHTML = `<img class="movie__cover" src = "${ film.posterUrl }"  alt = ''>`
+        filmName.innerHTML = `${ film.nameRu } (${ film.year })`;
 
-        filmRating.classList.add('film-rating');
-        filmPoster.classList.add('film-poster');
-        filmName.classList.add('film-name');
-        filmYear.classList.add('film-year');
+        filmCard.classList.add('movie');
+        filmRating.classList.add('movie__average');
+        filmPoster.classList.add('movie__cover-inner');
+        filmName.classList.add('movie__title');
 
-        filmCard.append(filmRating, filmName, filmPoster, filmYear)
+
+
+        filmCard.append(filmPoster, filmName, filmRating)
 
         filmsList.append(filmCard);
     }
 }
 
+function changePage() {
+    let pageList = document.getElementsByClassName('page')
+
+    async function change(event) {
+        count = event.target.innerText
+        await getFilms()
+
+        for (let page of pageList) {
+            page.classList.remove('active')
+        }
+
+        event.target.classList.add('active')
+        window.scrollTo(0, 0);
+    }
+
+    for (let page of pageList) {
+
+        page.addEventListener('click', change)
+    }
+}
+
+
 getFilms();
+changePage()
