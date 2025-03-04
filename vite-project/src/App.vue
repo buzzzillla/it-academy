d3549336dff0c7e7de0d0e0dd34ba40c518a2f2c
ilogic-8-a <urypin2010@gmail.com>
2025-03-04 17:59:19 +0300

    решали контрольную

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -2,10 +2,14 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import CompanyCard from "./components/company-card.vue";
 let API_KEY = 'ba0235c4d6ba3983024e60f0b3143eddf61513a8';
 // создание переменной с помощью ref из фреймворка vue.js
 let search = ref('');
 let companies = ref([]);
+
+// выбранная пользователем организация
+let selectedCompany = ref(null);
 
 // функция для получения
 async function getCompanies() {
@@ -26,67 +30,89 @@ async function getCompanies() {
   // ПОЛУЧЕНИЕ ЗНАЧЕНИЯ ПЕРЕМЕННОЙ  (Hi OPi HA MiNЯ)
   companies.value = response.data.suggestions
 }
+  // функция для выбора организации
+  async function selectCompany(company) {
+        selectedCompany.value = company;
+}
 
+// функция для возвращения к форме поиска организации
+async function goBack() {
+      selectedCompany.value = null;
+}
+
+function deleteCompany(index) {
+  companies.value.splice(index, 1)
+}
 
 </script>
 
 <template>
   <div class="app">
     <div class="companies-card">
-      <h1 class="card-title">
-        Поиск организации
-      </h1>
+      <transition name="mode-fade" mode="out-in">
+        <CompanyCard
+            @goBack="goBack"
+          v-if="selectedCompany"
+          :selected-company="selectedCompany"
+          />
+        <div v-else>
+          <h1 class="card-title">
+            Поиск организации
+          </h1>
 
+          <div class="search-form">
+            <input
+                v-model="search"
+                class="search-input"
+                id="search-input"
+                @keyup.enter="getCompanies"
+                placeholder="Напишите название организации">
+            <button
+                class="search-button"
+                id="search-button"
+                @click="getCompanies">Найти
+            </button>
+          </div>
 
-      <div class="search-form">
-        <input
-            v-model="search"
-            class="search-input"
-            id="search-input"
-            @keyup.enter="getCompanies"
-            placeholder="Напишите название организации">
-        <button
-            class="search-button"
-            id="search-button"
-            @click="getCompanies">Найти</button>
-      </div>
+          <div class="companies-list">
+            <div class="companies-title">
+              Найденные организации:
+            </div>
 
-
-
-      <div class="companies-list">
-        <div class="companies-title">
-          Найденные организации:
-        </div>
-
-        <div
-            v-if="companies.length > 0"
-
-            class="companies">
-          <div
-              class="company"
-              v-for="(company, index) in companies"
-              :class="{
+            <div
+                v-if="companies.length > 0"
+                class="companies"
+            >
+              <div
+                  class="company"
+                  @click="selectCompany(company)"
+                  v-for="(company, index) in companies"
+                  :class="{
               'active-company': company.data.state.status === 'ACTIVE',
               'inactive-company': company.data.state.status !== 'ACTIVE',
             }"
-            >
-            <div class="company-name">
-              {{index + 1}}. {{company.value}}
-              </div>
-              <div class="company-inn">
-              ИНН: {{company.data.inn}}
-              </div>
-                <div class="company-address">
-             АДРЕС:  {{company.data.address.value}}
+              >
+                <div class="company-name">
+                  {{ index + 1 }}. {{ company.value }}
                 </div>
+                <div class="company-inn">
+                  ИНН: {{ company.data.inn }}
+                </div>
+                <div class="company-address">
+                  АДРЕС: {{ company.data.address.value }}
+                </div>
+                <button @click.stop="deleteCompany(index)">Удалить</button>
               </div>
             </div>
-        <div v-else>
-          Напишите название организации в текстовое поле и нажмите кнопку "Найти".
-        </div>
+            <div v-else>
+              Напишите название организации в текстовое поле и нажмите кнопку "Найти".
+            </div>
           </div>
         </div>
-     </div>
+      </transition>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -97,4 +123,14 @@ async function getCompanies() {
 .inactive-company {
     background-color: #ffd4d4;
 }
+
+.mode-fade-enter-active, .mode-fade-leave-active {
+  transition:opacity .3s ease
+}
+
+.mode-fade-enter-from, .mode-fade-leave-to {
+  opacity: 0;
+}
 </style>
+
+
---
+++ vite-project/src/components/company-card.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+let props = defineProps({
+  selectedCompany: {
+    type: Object,
+  },
+});
+
+let emit = defineEmits(['goBack']);
+// функция для возвращения назад
+function goBack() {
+    emit('goBack')
+}
+
+
+</script>
+
+<template>
+  <div class="company-card">
+    <h1 class="card-title">
+      {{selectedCompany.value}}
+    </h1>
+    <div
+        :class="{
+              'active': selectedCompany.data.state.status === 'ACTIVE',
+              'inactive': selectedCompany.data.state.status !== 'ACTIVE',
+            }"
+        class="company-card__status">
+
+      {{  selectedCompany.data.state.status === 'ACTIVE' ? "Действующая организация" : "Организация ликвидирована" }}
+    </div>
+    <div class="company-card__property">
+      ОГРН организации: {{ selectedCompany.data.ogrn }}
+    </div>
+
+    <div class="company-card__property">
+      ИНН организации: {{ selectedCompany.data.inn }}
+    </div>
+
+    <div class="company-card__property">
+      КПП организации:  {{ selectedCompany.data.kpp }}
+    </div>
+
+    <div class="company-card__property">
+      Дата регистрации:  {{ new Date(selectedCompany.data.state.registration_date).toLocaleDateString() }}
+    </div>
+
+    <div class="company-card__property">
+      Адрес организации:  {{ selectedCompany.data.address.value }}
+    </div>
+
+    <div class="company-card__property">
+      Руководитель:  {{ selectedCompany.data.management.name }}  ({{ selectedCompany.data.management.post.toLowerCase() }})
+    </div>
+    <button
+        @click="goBack"
+        class="company-card__button">Вернуться назад</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
